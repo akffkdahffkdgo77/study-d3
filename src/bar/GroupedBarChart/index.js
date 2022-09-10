efb62d7c75f1f0edd8f4926a7656bbb8a8d4687d
gykim <52883505+namiein@users.noreply.github.com>
2022-09-10 11:50:29 +0000

    chore : import 절대 경로로 수정

--- src/bar/GroupedBarChart/index.js
+++ src/bar/GroupedBarChart/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Component from './Component';
-import { data, options } from './data';
+import Component from 'bar/GroupedBarChart/Component';
+import { data, options } from 'bar/GroupedBarChart/data';
 
 export default function GroupedBarChart() {
     return <Component data={data} options={options} />;
--- src/bar/StackedBarChart/index.js
+++ src/bar/StackedBarChart/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Component from './Component';
-import { data, options } from './data';
+import Component from 'bar/StackedBarChart/Component';
+import { data, options } from 'bar/StackedBarChart/data';
 
 export default function StackedBarChart() {
     return <Component data={data} options={options} />;
