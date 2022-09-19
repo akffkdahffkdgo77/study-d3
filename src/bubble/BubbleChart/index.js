2b896bcca3cb7d1775bfe1c61ad84504fde69cd4
gykim <52883505+namiein@users.noreply.github.com>
2022-09-19 08:08:03 +0000

    mv : Bubble Chart 관련된 css -> bubble 폴더로 이동

--- src/bubble/BubbleChart/index.js
+++ src/bubble/BubbleChart/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Component from 'bubble/BubbleChart/Component';
 import { data, options } from 'bubble/BubbleChart/data';
+import 'bubble/styles/index.css';
 
 export default function BubbleChart() {
     return <Component data={data} options={options} />;
